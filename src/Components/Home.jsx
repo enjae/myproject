84cf0402831d4dba181873e0bfc6ece4d55c2194
Nandini Jaryal <jarialnandini1911@gmail.com>
2023-08-29 21:55:42 +0530

    v4.5

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -348,7 +348,7 @@ const Home = () => {
               <path fill="none" d="M0 0h24v24H0z" />
               <path d="M19 19H5V5h7V3H5a2 2 0 00-2 2v14a2 2 0 002 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" />
             </svg>
-            <div className="flex flex-col">
+            <div className="flex flex-col mx-4">
                   <Link to="/NandiniJaryal-Projects" className="text-white text-sm md:text-xl lg:text-3xl font-bold">
                     Projects
                   </Link>
